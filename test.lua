c1ea241094939de615dc005acba92f27a14fc716
STEEVE51 <51879645+STEEVE51@users.noreply.github.com>
2025-04-28 17:30:28 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -6,7 +6,7 @@ NewSpaceConfig.BossMenu = {
     Positions = {
         ['police'] = vector3(433.8358, -978.2630, 30.7096),
         ['ambulance'] = vector3(335.46, -594.52, 43.28),
-        ['mechanic'] = vector3(413.6291, -997.0977, 29.3713),
+        ['mechanic'] = vector3(408.3563, -972.0017, 29.3651),
         ['taxi'] = vector3(907.24, -160.26, 74.17)
     },
     
