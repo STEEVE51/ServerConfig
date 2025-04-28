933380b2bb8df7b71fd433a2885273c73d8edff1
STEEVE51 <51879645+STEEVE51@users.noreply.github.com>
2025-04-28 15:27:07 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,12 @@
-local data = "Si tu vois ceci c'est que j'ai réussit a récupérer la config depuis github"
+NewSpaceConfig = {}
 
-local pricesandwitch = 20
+
+NewSpaceConfig.BossMenu = {
+
+    Positions = {
+        ['police'] = vector3(433.8358, -978.2630, 30.7096),
+        ['ambulance'] = vector3(335.46, -594.52, 43.28),
+        ['mechanic'] = vector3(419.2987, -990.7612, 29.3233),
+        ['taxi'] = vector3(907.24, -160.26, 74.17)
+    },
+},
