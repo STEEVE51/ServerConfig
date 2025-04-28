cf963903a5587c180f61b351dcf8f6595f7de204
STEEVE51 <51879645+STEEVE51@users.noreply.github.com>
2025-04-28 16:22:00 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,11 +2,20 @@ NewSpaceConfig = {}
 
 
 NewSpaceConfig.BossMenu = {
-
+    -- tu peux ajouter des jobs ici
     Positions = {
         ['police'] = vector3(433.8358, -978.2630, 30.7096),
         ['ambulance'] = vector3(335.46, -594.52, 43.28),
         ['mechanic'] = vector3(413.6291, -997.0977, 29.3713),
         ['taxi'] = vector3(907.24, -160.26, 74.17)
-    }
+    },
+    
+     -- Grades requis pour accéder au menu boss
+    RequiredGrades = {
+        ['police'] = 4,
+        ['ambulance'] = 3,
+        ['mechanic'] = 3,
+        ['taxi'] = 2
+    },
+    
 }
