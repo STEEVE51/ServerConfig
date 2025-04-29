c61ab5cfaece45ba78ad9af7db4e374b5cb4094e
MagikWhiteeee <ducoudretlouis@gmail.com>
2025-04-29 20:01:37 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -4,10 +4,17 @@ NewSpaceConfig = {}
 NewSpaceConfig.BossMenu = {
     -- tu peux ajouter des jobs ici
     Positions = {
-        ['police'] = vector3(104.6285, -958.7578, 29.7254),
+        ['police'] = vector3(2511.7, -423.06, 106.91),
         ['ambulance'] = vector3(335.46, -594.52, 43.28),
-        ['mechanic'] = vector3(408.3563, -972.0017, 29.3651),
+        ['mechanic'] = vector3(-348.21, -131.1, 42.04),
+        ['mechanic1'] = vector3(1187.04, 2637.39, 38.4),
         ['taxi'] = vector3(907.24, -160.26, 74.17),
+        ['gouv'] = vector3(-383.99, 1075.94, 334.89),
+        ['pizeria'] = vector3(-1180.4, -1408.57, 10.52),
+        ['uwu'] = vector3(-596.91, -1053.35, 22.34),
+        ['bahamas'] = vector3(-1376.24, -621.61, 35.9),
+        ['vanilla'] = vector3(112.42, -1320.51, 24.72),
+        ['flamingo'] = vector3(-2326.76, -534.2, 8.92),
         
     },
     
